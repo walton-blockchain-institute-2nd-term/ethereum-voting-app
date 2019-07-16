2da7cecb65203e43fe86a0020ab86b3e3eea1fbb
ch-4ml <ark9659@gmail.com>
2019-07-16 19:01:19 +0900

    reset

--- chapter2/app/javascripts/app.js
+++ chapter2/app/javascripts/app.js
@@ -4,7 +4,6 @@ import "../stylesheets/app.css";
 import { default as Web3} from 'web3';
 import { default as contract } from 'truffle-contract'
 
-
 import voting_artifacts from '../../build/contracts/Voting.json'
 
 var Voting = contract(voting_artifacts);
@@ -37,16 +36,11 @@ window.voteForCandidate = function(candidate) {
 $( document ).ready(function() {
   if (typeof web3 !== 'undefined') {
     console.warn("Using web3 detected from external source like Metamask")
-
-//////////////////////////////////////////////////////////////////
-/////
+    window.web3 = new Web3(web3.currentProvider);
   } else {
     console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
-    
-    /////
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
   }
-  //////////////////////////////////////////////////////////////////
-  
   
     web3.eth.getAccounts(function (err, accs) {
       if (err != null) {
--- chapter2/migrations/2_deploy_contracts.js
+++ chapter2/migrations/2_deploy_contracts.js
@@ -1,4 +1,4 @@
 var Voting = artifacts.require("./Voting.sol");
 module.exports = function(deployer) {
-  deployer.deploy(Voting, [/////////////////////]);
+  deployer.deploy(Voting, ["0x52616d61", "0x4e69636b", "0x4a6f7365"]);
 };
